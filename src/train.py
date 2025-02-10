8325c9c8ba5ba7df4324b01be396b5302998f7b4
Linus Salzmann <linus.salzmann@tum.de>
2025-02-10 18:16:02 +0100

    feat(training): enhance training process with mixed precision and gradient accumulation, update .gitignore

--- src/train.py
+++ src/train.py
@@ -15,6 +15,7 @@ from utils.train_utils import (
     JSONStoppingCriteria,
 )
 from torchmetrics.detection.mean_ap import MeanAveragePrecision
+from torch.cuda.amp import GradScaler, autocast
 
 
 MODEL_NAME = "lmms-lab/llava-onevision-qwen2-0.5b-si"
@@ -43,7 +44,7 @@ class Trainer:
         scheduler,
         device,
         gradient_accumulation_steps=4,
-        max_grad_norm=1.0,
+        max_grad_norm=None,  # 1.0,
         checkpoint_dir="checkpoints",
     ):
         self.model = model
@@ -53,9 +54,9 @@ class Trainer:
         self.scheduler = scheduler
         self.device = device
         self.checkpoint_dir = checkpoint_dir
-        
-        #self.gradient_accumulation_steps = gradient_accumulation_steps
-        #self.max_grad_norm = max_grad_norm
+
+        self.gradient_accumulation_steps = gradient_accumulation_steps
+        self.max_grad_norm = max_grad_norm
 
         # Initialize metric once
         self.metric = MeanAveragePrecision(box_format="xyxy", iou_type="bbox").to(
@@ -63,10 +64,9 @@ class Trainer:
         )
 
         # Mixed precision
-        #self.scaler = GradScaler()
+        self.scaler = GradScaler()
 
         os.makedirs(checkpoint_dir, exist_ok=True)
-
 
     def train_epoch(self, epoch, num_epochs):
         self.model.train()
@@ -74,7 +74,9 @@ class Trainer:
         running_loss = 0
 
         # add tqdm
-        progress_bar = tqdm(self.train_dataloader, desc=f"Train/Epoch {epoch+1}/{num_epochs}")
+        progress_bar = tqdm(
+            self.train_dataloader, desc=f"Train/Epoch {epoch+1}/{num_epochs}"
+        )
 
         for step, batch in enumerate(progress_bar):
             # Move batch to device
@@ -86,44 +88,61 @@ class Trainer:
             # Prepare lables
             labels = input_ids.clone()
             # TODO: make image_token_id as attribute
-            image_token_id = self.model.tokenizer.convert_tokens_to_ids(self.model.image_token)
+            image_token_id = self.model.tokenizer.convert_tokens_to_ids(
+                self.model.image_token
+            )
             labels[labels == image_token_id] = -100  # Mask image tokens
             labels[loss_masks == 0] = -100  # Mask everything except the answer tokens
 
             # Forward pass
             self.optimizer.zero_grad()
-            outputs = self.model(
-                input_ids=input_ids,
-                attention_mask=attention_mask,
-                images=images,
-                labels=labels,
-            )
-            loss = outputs.loss
 
-            loss.backward()
-            self.optimizer.step()
-            self.scheduler.step()
+            with autocast():
+                outputs = self.model(
+                    input_ids=input_ids,
+                    attention_mask=attention_mask,
+                    images=images,
+                    labels=labels,
+                )
+                loss = outputs.loss / self.gradient_accumulation_steps
 
-            total_loss += loss.item()
+            self.scaler.scale(loss).backward()
+
+            if (step + 1) % self.gradient_accumulation_steps == 0:
+                # unscale gradients
+                if self.max_grad_norm is not None:  # grad_clip_norm
+                    self.scaler.unscale_(optimizer)
+                    torch.nn.utils.clip_grad_norm_(
+                        model.parameters(), self.max_grad_norm
+                    )
+
+                self.scaler.step(self.optimizer)
+                self.scaler.update()
+                self.scheduler.step()
+                self.optimizer.zero_grad()
+
+            total_loss += loss.item() * self.gradient_accumulation_steps
             running_loss = total_loss / (step + 1)
 
-            progress_bar.set_postfix({"loss": running_loss})
+            # progress_bar.set_postfix({"loss": running_loss})
             wandb.log(
                 {
                     "train/step_loss": running_loss,
-                    "train/learning_rate": scheduler.get_last_lr()[0],
+                    "train/learning_rate": self.scheduler.get_last_lr()[0],
                 }
             )
 
+        progress_bar.close()
         return total_loss / len(self.train_dataloader)
-
 
     @torch.inference_mode()
     def evaluate(self, epoch, num_epochs):
         self.model.eval()
         self.metric.reset()
 
-        progress_bar = tqdm(self.val_dataloader, desc=f"Eval/Epoch {epoch+1}/{num_epochs}")
+        progress_bar = tqdm(
+            self.val_dataloader, desc=f"Eval/Epoch {epoch+1}/{num_epochs}"
+        )
 
         for batch in progress_bar:
             # TODO: maybe better to patch images for localization performance
@@ -140,13 +159,17 @@ class Trainer:
             )
 
             # Decode predictions
-            generated_text = self.model.tokenizer.batch_decode(outputs, skip_special_tokens=True)
+            generated_text = self.model.tokenizer.batch_decode(
+                outputs, skip_special_tokens=True
+            )
 
             # print(generated_text)
 
             # Parse model ouput to bbox and lables
             predicted_boxes = [
-                parse_model_output_to_boxes(generated_text, self.val_dataloader.dataset, i, device)
+                parse_model_output_to_boxes(
+                    generated_text, self.val_dataloader.dataset, i, device
+                )
                 for i in range(len(batch["instance_bboxes"]))
             ]
 
@@ -175,7 +198,6 @@ class Trainer:
             "map_75": metrics["map_75"].item(),
         }
 
-
     def save_checkpoint(self, epoch, metrics, is_best=False):
         checkpoint = {
             "epoch": epoch,
@@ -185,7 +207,9 @@ class Trainer:
             "metrics": metrics,
         }
 
-        checkpoint_path = os.path.join(self.checkpoint_dir, f"checkpoint_{epoch}_{int(time.time())}.pt")
+        checkpoint_path = os.path.join(
+            self.checkpoint_dir, f"checkpoint_{epoch}_{int(time.time())}.pt"
+        )
         torch.save(checkpoint, checkpoint_path)
 
         if is_best:
@@ -194,14 +218,12 @@ class Trainer:
 
         # _cleanup_old_checkpoints()
 
-
     def _cleanup_old_checkpoints(self, keep_last_n=3):
         checkpoints = sorted(
             [f for f in os.listdir(self.checkpoint_dir) if f.startswith("checkpoint_")]
         )
         for checkpoint in checkpoints[:-keep_last_n]:
             os.remove(os.path.join(self.checkpoint_dir, checkpoint))
-
 
     def train(self, num_epochs=5):
         best_map = 0
@@ -211,7 +233,7 @@ class Trainer:
             train_loss = self.train_epoch(epoch, num_epochs)
 
             # Validation
-            val_metrics = self.evaluate( epoch, num_epochs)
+            val_metrics = self.evaluate(epoch, num_epochs)
 
             print(val_metrics)
             wandb.log(
@@ -233,14 +255,15 @@ class Trainer:
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = torch.device("cuda")
 # device = torch.device("mps" if torch.backends.mps.is_available() else device)
 print(f"Using device: {device}")
 
 model = VisionLanguageModel(model_name=MODEL_NAME).to(device)
 
 # Create dataloader
-train_dataloader = build_train_dataloader(model, batch_size=4, num_samples=100)
-val_dataloader = build_val_dataloader(model, batch_size=4, num_samples=20)
+train_dataloader = build_train_dataloader(model, batch_size=2, num_samples=100)
+val_dataloader = build_val_dataloader(model, batch_size=2, num_samples=20)
 
 # Freeze all layers except projection layer and new token embeddings
 for param in model.parameters():
@@ -267,13 +290,13 @@ scheduler = get_scheduler(
 trainer = Trainer(
     model=model,
     train_dataloader=train_dataloader,
-    val_dataloader=val_dataloader, 
+    val_dataloader=val_dataloader,
     optimizer=optimizer,
     scheduler=scheduler,
     device=device,
-    checkpoint_dir="/u/home/salzmann/Documents/dev/checkpoints"
-    #gradient_accumulation_steps=4,
-    #max_grad_norm=1.0
+    checkpoint_dir="/u/home/salzmann/Documents/dev/checkpoints",
+    gradient_accumulation_steps=4,
+    # max_grad_norm=1.0
 )
 
 # Train model
