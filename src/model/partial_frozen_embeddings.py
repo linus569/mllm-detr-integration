9e736aa7e62c65369f873df7571f24244c6c22fa
Linus Salzmann <salzmann.linus@gmail.com>
2025-03-05 09:37:03 +0100

    fix(model): remove destination from state_dict function, format file

--- src/model/partial_frozen_embeddings.py
+++ src/model/partial_frozen_embeddings.py
@@ -3,13 +3,18 @@ from torch import nn
 
 
 class PartiallyFrozenEmbedding(nn.Module):
-    def __init__(self, frozen_embedding: nn.Embedding, 
-                 new_tokens: int, 
-                 initializers: list[list[int]],
-                 do_init: bool = True):
+    def __init__(
+        self,
+        frozen_embedding: nn.Embedding,
+        new_tokens: int,
+        initializers: list[list[int]],
+        do_init: bool = True,
+    ):
         super().__init__()
         self.frozen_embedding = frozen_embedding
-        self.trainable_embedding = nn.Embedding(new_tokens, frozen_embedding.embedding_dim)
+        self.trainable_embedding = nn.Embedding(
+            new_tokens, frozen_embedding.embedding_dim
+        )
         self.num_frozen = frozen_embedding.num_embeddings
         self.num_trainable = new_tokens
         self.num_embeddings = self.num_frozen + self.num_trainable
@@ -22,20 +27,22 @@ class PartiallyFrozenEmbedding(nn.Module):
         for param in self.frozen_embedding.parameters():
             param.requires_grad = False
 
-    def state_dict(self, destination=None, prefix='', keep_vars=False):
-        return self.trainable_embedding.state_dict(destination, prefix, keep_vars)
-    
+    def state_dict(self, prefix="", keep_vars=False):
+        return self.trainable_embedding.state_dict(prefix=prefix, keep_vars=keep_vars)
+
     def load_state_dict(self, state_dict, strict=True):
         return self.trainable_embedding.load_state_dict(state_dict, strict)
 
     def _init_trainable_embedding(self, initializers: list[list[int]]):
-        assert len(initializers) == self.num_trainable, "Initializers should be provided for all new tokens"
+        assert (
+            len(initializers) == self.num_trainable
+        ), "Initializers should be provided for all new tokens"
         old_embeddings_weight = self.frozen_embedding.weight.data.to(torch.float32)
 
-        new_embeddings_weight = _init_new_embeddings(old_embeddings_weight, initializers,
-                                                        add_1std_noise_for_empty_init=True)
+        new_embeddings_weight = _init_new_embeddings(
+            old_embeddings_weight, initializers, add_1std_noise_for_empty_init=True
+        )
         self.trainable_embedding.weight.data = new_embeddings_weight
-
 
     def forward(self, input_ids: torch.LongTensor) -> torch.FloatTensor:
         N, L = input_ids.shape
@@ -49,24 +56,29 @@ class PartiallyFrozenEmbedding(nn.Module):
         embeddings[frozen_mask, :] = frozen_embed
         embeddings[~frozen_mask, :] = trainable_embed
 
-        #frozen_input_ids = input_ids.clamp(max=self.num_frozen - 1)
-        #trainable_input_ids = input_ids.clamp(min=self.num_frozen) - self.num_frozen
-        #frozen_embed = self.frozen_embedding(frozen_input_ids)
-        #trainable_embed = self.trainable_embedding(trainable_input_ids)
-        #embeddings = torch.where(frozen_mask.unsqueeze(-1), frozen_embed, trainable_embed)
+        # frozen_input_ids = input_ids.clamp(max=self.num_frozen - 1)
+        # trainable_input_ids = input_ids.clamp(min=self.num_frozen) - self.num_frozen
+        # frozen_embed = self.frozen_embedding(frozen_input_ids)
+        # trainable_embed = self.trainable_embedding(trainable_input_ids)
+        # embeddings = torch.where(frozen_mask.unsqueeze(-1), frozen_embed, trainable_embed)
 
         return embeddings
-    
-    
+
+
 class PartiallyFrozenLMHead(nn.Module):
-    def __init__(self, frozen_lm_head: nn.Linear, 
-                 new_tokens: int, 
-                 initializers: list[list[int]],
-                 do_init: bool = True):
+    def __init__(
+        self,
+        frozen_lm_head: nn.Linear,
+        new_tokens: int,
+        initializers: list[list[int]],
+        do_init: bool = True,
+    ):
         super().__init__()
 
         self.frozen_lm_head = frozen_lm_head
-        self.trainable_lm_head = nn.Linear(frozen_lm_head.in_features, new_tokens, bias=frozen_lm_head.bias is not None)
+        self.trainable_lm_head = nn.Linear(
+            frozen_lm_head.in_features, new_tokens, bias=frozen_lm_head.bias is not None
+        )
         self.num_frozen = frozen_lm_head.out_features
         self.num_trainable = new_tokens
         self.in_features = frozen_lm_head.in_features
@@ -78,24 +90,28 @@ class PartiallyFrozenLMHead(nn.Module):
         for param in self.frozen_lm_head.parameters():
             param.requires_grad = False
 
-    def state_dict(self, destination=None, prefix='', keep_vars=False):
-        return self.trainable_lm_head.state_dict(destination, prefix, keep_vars)
-    
+    def state_dict(self, prefix="", keep_vars=False):
+        return self.trainable_lm_head.state_dict(prefix=prefix, keep_vars=keep_vars)
+
     def load_state_dict(self, state_dict, strict=True):
         return self.trainable_lm_head.load_state_dict(state_dict, strict)
 
     def _init_trainable_lm_head(self, initializers: list[list[int]]):
-        assert len(initializers) == self.num_trainable, "Initializers should be provided for all new tokens"
+        assert (
+            len(initializers) == self.num_trainable
+        ), "Initializers should be provided for all new tokens"
 
         old_lm_head_weight = self.frozen_lm_head.weight.data.to(torch.float32)
-        new_lm_head_weight = _init_new_embeddings(old_lm_head_weight, initializers,
-                                                    add_1std_noise_for_empty_init=True)
+        new_lm_head_weight = _init_new_embeddings(
+            old_lm_head_weight, initializers, add_1std_noise_for_empty_init=True
+        )
         self.trainable_lm_head.weight.data = new_lm_head_weight
 
         if self.frozen_lm_head.bias is not None:
             old_lm_head_bias = self.frozen_lm_head.bias.data.to(torch.float32)
-            new_lm_head_bias = _init_new_embeddings(old_lm_head_bias, initializers,
-                                                    add_1std_noise_for_empty_init=True)
+            new_lm_head_bias = _init_new_embeddings(
+                old_lm_head_bias, initializers, add_1std_noise_for_empty_init=True
+            )
             self.trainable_lm_head.bias.data = new_lm_head_bias
 
     def forward(self, hidden_states: torch.FloatTensor) -> torch.FloatTensor:
@@ -105,7 +121,12 @@ class PartiallyFrozenLMHead(nn.Module):
         logits = torch.cat([frozen_logits, trainable_logits], dim=-1)
         return logits
 
-def _init_new_embeddings(old_embeddings, initializers: list[list[int]], add_1std_noise_for_empty_init: bool = True):
+
+def _init_new_embeddings(
+    old_embeddings,
+    initializers: list[list[int]],
+    add_1std_noise_for_empty_init: bool = True,
+):
     mean_embedding = old_embeddings.mean(dim=0)
     std_embedding = old_embeddings.std(dim=0)
 
@@ -130,7 +151,9 @@ if __name__ == "__main__":
     initializers = [[], [1], [2], [3], [4], [5], [6], [7], [8], [9]]
     do_init = True
 
-    partial_frozen_embedding = PartiallyFrozenEmbedding(frozen_embedding, new_tokens, initializers, do_init)
+    partial_frozen_embedding = PartiallyFrozenEmbedding(
+        frozen_embedding, new_tokens, initializers, do_init
+    )
     input_ids = torch.randint(0, 110, (32, 128))
     embeddings = partial_frozen_embedding(input_ids)
     print(embeddings.shape)
@@ -140,7 +163,9 @@ if __name__ == "__main__":
     initializers = [[], [1], [2], [3], [4], [5], [6], [7], [8], [9]]
     do_init = True
 
-    partial_frozen_lm_head = PartiallyFrozenLMHead(frozen_lm_head, new_tokens, initializers, do_init)
+    partial_frozen_lm_head = PartiallyFrozenLMHead(
+        frozen_lm_head, new_tokens, initializers, do_init
+    )
     hidden_states = torch.randn(32, 128, 128)
     logits = partial_frozen_lm_head(hidden_states)
     print(logits.shape)
